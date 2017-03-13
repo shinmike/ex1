af361eea442e98cfa4a3145170e9d82a4ba0e398
shinmike <shinmike90@gmail.com>
2017-03-13 21:38:28 +0000

    second step complete

--- step2.js
+++ step2.js
@@ -1,28 +1,30 @@
-function getAndPrintHTMLChunks () {
+function getAndPrintHTML () {
 
   var requestOptions = {
     host: 'sytantris.github.io',
-    path: '/http-examples/step1.html'
+    path: '/http-examples/step2.html'
   };
 
   var https = require('https');
+
+  var space = [];
 
   https.get(requestOptions, function (response) {
 
     response.setEncoding('utf8');
 
     response.on('data', function (data) {
-      console.log('Chunk Received. Length:', data.toString() + '\n');
+      space.push(data);
+      console.log(space.join(""));
+// console.log can be done here or end
     });
 
     response.on('end', function() {
-      console.log('Response stream complete.')
+// console.log(space.join(""))
     });
 
-});
+}); return getAndPrintHTML;
 
 };
 
-// HA HA HA JEREMY WAS HERE, YOU GOT VANDALIZED!!!
-
-getAndPrintHTMLChunks();
+getAndPrintHTML();
