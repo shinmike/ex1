3eb2d9aa350ed48f3e77c7fe545a8a3e0a14935d
shinmike <shinmike90@gmail.com>
2017-04-18 00:16:54 +0000

    step 4 revised solution

--- step4.js
+++ step4.js
@@ -1,34 +1,39 @@
-var https = require('https');
-
-//Second Step within the program    -     2nd Step
+// 1. getHTML function defined. Includes parameters 'options' and 'callback'
 function getHTML (options, callback) {
 
-  https.get(options, function (response) {
+  var https = require('https');
 
-    var space = [];
+  var space = "";
+
+  https.get(options, function (response) {
 
     response.setEncoding('utf8');
 
     response.on('data', function (data) {
-      space.push(data);
+      space += data;
+      // printHTML function called and space string is the parameter 'html'
+      // and console.logs this
+      callback(space);
     });
 
     response.on('end', function() {
-      callback(space.join(""));
+      console.log("Response stream complete!");
     });
 
   });
 
-};
-
-function printHTML (html) {
-  console.log(html);
 }
 
+// this is the 'options'
 var requestOptions = {
   host: 'sytantris.github.io',
   path: '/http-examples/step4.html'
 };
 
-//This is the start of the Program    1st Step
+// this is the 'callback' function
+function printHTML (html) {
+  console.log(html);
+}
+
+// 2. Calls getHTML function, passing in requestOptions (options) and printHTML (callback)
 getHTML(requestOptions, printHTML);
